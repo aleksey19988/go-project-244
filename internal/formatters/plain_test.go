99db0c199099bf696eefcf9a61e9e0699a6e01ca
a.kononenko <a.knnnk@mail.ru>
2026-06-12 13:53:32 +0500

    Рекурсивное сравнение - Добавил тесты для plain formatter

--- internal/formatters/plain_test.go
+++ internal/formatters/plain_test.go
@@ -2,6 +2,7 @@ package formatters
 
 import (
 	"code/internal/diff"
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -24,22 +25,66 @@ func TestFormat(t *testing.T) {
 
 	res, err := pf.Format(fields)
 	assert.NoError(t, err)
-	assert.Equal(t, "Property 'Name 1' was removed\n", res)
+	expected := fmt.Sprintf("Property '%s' was removed\n", fields[0].Name)
+	assert.Equal(t, expected, res)
 }
 
 func TestGetOutputString(t *testing.T) {
-	fld := diff.Field{
-		Name:     "Name 2",
-		Status:   Added,
-		OldValue: nil,
-		NewValue: "456",
-		Depth:    1,
-		Children: nil,
-	}
+	t.Run("Zero deep error", func(t *testing.T) {
+		fld := diff.Field{
+			Name:     "Name 2",
+			Status:   Added,
+			OldValue: nil,
+			NewValue: "456",
+			Depth:    0,
+			Children: nil,
+		}
+		f, err := NewFormatter(PlainOutputFormat)
+		assert.NoError(t, err)
+		assert.NotNil(t, f)
 
-	f, err := NewFormatter(PlainOutputFormat)
-	assert.NoError(t, err)
-	res, err := f.GetOutputString(fld)
-	assert.NoError(t, err)
-	assert.Equal(t, "Property 'Name 2' was added with value: '456'\n", res)
+		_, err = f.GetOutputString(fld)
+		assert.Error(t, err)
+		assert.Equal(t, "deep is negative or zero", err.Error())
+	})
+
+	t.Run("Add field", func(t *testing.T) {
+		fld := diff.Field{
+			Name:     "Name 3",
+			Status:   Added,
+			OldValue: nil,
+			NewValue: "456",
+			Depth:    1,
+			Children: nil,
+		}
+
+		f, err := NewFormatter(PlainOutputFormat)
+		assert.NoError(t, err)
+		assert.NotNil(t, f)
+
+		res, err := f.GetOutputString(fld)
+		assert.NoError(t, err)
+		expected := fmt.Sprintf("Property '%s' was added with value: '%v'\n", fld.Name, fld.NewValue)
+		assert.Equal(t, expected, res)
+	})
+
+	t.Run("Remove field", func(t *testing.T) {
+		fld := diff.Field{
+			Name:     "Name 2",
+			Status:   Removed,
+			OldValue: nil,
+			NewValue: "456",
+			Depth:    1,
+			Children: nil,
+		}
+
+		f, err := NewFormatter(PlainOutputFormat)
+		assert.NoError(t, err)
+		assert.NotNil(t, f)
+
+		res, err := f.GetOutputString(fld)
+		assert.NoError(t, err)
+		expected := fmt.Sprintf("Property '%s' was removed\n", fld.Name)
+		assert.Equal(t, expected, res)
+	})
 }
